c5bdeda6756df210e3aae07d817db43ee80c6e71
Calvin <calmoon808@gmail.com>
2019-07-21 16:31:14 -1000

    changed remove

--- linkedList.js
+++ linkedList.js
@@ -43,20 +43,20 @@ function linkedListGenerator(){
         },
         remove: function(index){
             let current = this.get(index);
-            let before;
+            let before = this.get(index - 1);
             if (current === false){
                 return false;
             }
-            if (index <= 0){
+            if (index === 0){
                 head = current.next;
                 before = false;
             } else if (!this.get(index + 1)) {
-                before = this.get(index - 1);
                 tail = before;
+                before.next = null;
             } else {
                 before = this.get(index - 1);
+                before.next = current.next;
             }
-            before.next = current.next;
         },
         insert: function(value, index){
             if (!this.get(index)){
